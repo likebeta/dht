53beb6bd11e38aff0ece18b948b0a6aa35ab9760
likebeta <ixxoo.me@gmail.com>
2016-10-17 22:25:55 +0800

    update

--- tool/mp4ba.py
+++ tool/mp4ba.py
@@ -15,18 +15,40 @@ from util.log import Logger
 class Mp4Ba(object):
     URL = 'http://www.mp4ba.com'
 
-    def __init__(self, start=None, worker=3):
+    def __init__(self, start=None, worker=2):
         self.start = start
         self.worker = worker
 
+    def fetch_pages(self, *urls):
+        if len(urls) == 1:
+            Logger.debug('start fetch page:', urls[0])
+            return client.getPage(urls[0])
+        dl = []
+        for url in urls:
+            Logger.debug('start fetch page:', url)
+            dl.append(client.getPage(url))
+        if dl:
+            return defer.DeferredList(dl, consumeErrors=True)
+
     @defer.inlineCallbacks
     def run(self):
-        Logger.debug('start download index page:', self.URL)
-        html = yield client.getPage(self.URL)
-        Logger.debug('start parsing index page:', self.URL)
-        
+        html = yield self.fetch_pages(self.URL)
+        Logger.debug('start parse page:', self.URL)
+        pq = PyQuery(html)
+        total_page = int(pq('.pages .pager-last').text())
+        Logger.debug('total page:', total_page)
+        pages = range(1, total_page + 1)
+        groups = [pages[i:i + self.worker] for i in xrange(0, len(pages), self.worker)]
+        for group in groups:
+            urls = ['http://www.mp4ba.com/4545index.php?page=' + str(i) for i in group]
+            htmls = yield self.fetch_pages(*urls)
+            for success, html in htmls:
+                if success:
+                    self.parse_index_page(html)
+                else:
+                    Logger.debug('')
 
-    def parse_index_page(self):
+    def parse_index_page(self, html):
         pass
 
     def parse_list_page(self, page):
