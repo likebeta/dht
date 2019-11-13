02eb1e2fb1a5f93e6903c29b720f5799299f29ff
蝎子 <ixxoo.me@gmail.com>
2019-11-13 22:08:57 +0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -4,4 +4,4 @@
 # Author: 易思龙 <ixxoo.me@gmail.com>
 # Create: 2015-05-29
 
-apt-get install libxml2-dev libxslt1-dev python-dev mysql-server sphinxsearch gcc
+apt-get install libxml2-dev libxslt1-dev python-dev mysql-server sphinxsearch gcc libssl-dev
